79c4a176bbbeda3af3217c6d4c4aab6da87035d3
Jose Chavez <joseacb144@gmail.com>
2020-07-13 19:33:03 -0500

    update

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
+
+// Login Page
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/login_page.html");
-})
+});
 
-app.get("/home", function(req, res){
+app.post("/", function(req, res) {
+  console.log(req.body);
+
+    res.send("Thanks for loging in!");
+});
+
+
+
+
+// Home Page
+app.get("/home", function(req, res) {
   res.sendFile(__dirname + "/home_page.html");
-})
+});
 
-app.get("/textbook", function(req,res){
+app.get("/textbook", function(req, res) {
   res.send("Textbook Search");
-})
+});
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
